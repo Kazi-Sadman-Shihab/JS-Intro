1cf94d8d395033451aac1585f72d7ce8a88593c0
Kazi Sadman Shihab <kazinahin123@gmail.com>
2024-08-08 19:53:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* exp1 */
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB619";
+}
+/* exp2 */
+function showText2(){
+	document.getElementById('s_text2').innerHTML="Hello World";
+}
+/* exp3 */
+function showText3(){
+	document.getElementById('s_text3').innerHTML="Welcome";
+}
+/* exp4 */
+function showText4(){
+	document.getElementById('s_text4').innerHTML="Take Care";
+}
